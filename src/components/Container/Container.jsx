d1b724fa7516cc6c743cd3384337bec2252362a6
Rodion Matvieiev <your.email@example.com>
2024-07-14 10:32:04 -0400

    updated main and container.jsx

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,7 +1,12 @@
+import clsx from 'clsx';
 import css from './container.module.css';
 
-const Container = ({ children }) => {
-  return <div className={css.container}>{children}</div>;
+const Container = ({ children, addClass = '' }) => {
+  return <div className={clsx(css.container, addClass)}>{children}</div>;
 };
 
 export default Container;
+
+<Container addClass={clsx(css.desktop, css.mainWrapper)}>
+  <div></div>
+</Container>;
